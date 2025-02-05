feb3ff05a6c6169a2967ba7e4a8e44bca0c07b44
beatriz <bia.estevam.25@usp.br>
2025-02-05 15:54:56 -0300

    update majiq parser complete as module

--- merging_outputs/majiq_Parser.py
+++ merging_outputs/majiq_Parser.py
@@ -1,35 +1,21 @@
 #!/usr/bin/env python3
-
-import argparse
-
-# Using argparse to process inputs
-parser = argparse.ArgumentParser(
-    description='Merge and process MAJIQ output files')
-parser.add_argument('-p', '--psi_file', type=str,
-                    help='.psi.tsv output from MAJIQ PSI', required=True)
-parser.add_argument('-v', '--voila_file', type=str,
-                    help='.voila.tsv output from MAJIQ VOILA', required=True)
-parser.add_argument('-s', '--SRR', type=str,
-                    help='sample SRR', required=True)
-parser.add_argument('-d', '--output_dir', type=str,
-                    help='path to output directory', required=True)
-parser.add_argument('-f', '--output_prefix', type=str,
-                    help='prfix to output files', required=True)
-args = parser.parse_args()
-
-
-# Setting input files and paths
-psi_file = args.psi_file
-voila_file = args.voila_file
-output_prefix = f"{args.output_dir}/{args.output_prefix}"
-srr = args.SRR
-
-# init
-
-# functions
+# majiq_Parser.py
 
 
 def full_coord(exons_starts, exons_ends, junction_start, junction_end, seqid):
+    """
+    Define full coordinates of a splicing event based on exons coordinates and junction positions.
+
+    Args:
+        exons_starts (list): List of exon start positions.
+        exons_ends (list): List of exon end positions.
+        junction_start (int): Start position of the junction.
+        junction_end (int): End position of the junction.
+        seqid (str): Chromosome or sequence ID.
+
+    Returns:
+        str: Full coordinate string in the format "seqid:start,end-start,end".
+    """
     i = 0
     coord_first_exon_start = 0
     coord_first_exon_end = 0
@@ -38,25 +24,41 @@ def full_coord(exons_starts, exons_ends, junction_start, junction_end, seqid):
     coord_second_exon_end = 0
 
     for exon_start in exons_starts:
-        if exon_start <= junction_start < exon_start[i+1]:
-            # junction start in exon i exon region
+        if exon_start <= junction_start < exons_starts[i + 1]:
+            # Junction start is within this exon
             coord_first_exon_start = exon_start
             coord_first_exon_end = junction_start
 
-        if exon_start <= junction_end < exon_start[i+1]:
+        if exon_start <= junction_end < exons_starts[i + 1]:
+            # Junction end is within this exon
             coord_second_exon_start = junction_end
-            if junction_end <= exons_ends[i]:  # in exon
+            if junction_end <= exons_ends[i]:  # Junction ends within the exon
                 coord_second_exon_end = exons_ends[i]
-            else:  # in intron
-                coord_second_exon_end = exons_ends[i+1]
+            else:  # Junction ends in the intron
+                coord_second_exon_end = exons_ends[i + 1]
         i += 1
 
     full_coordinate = f"{seqid}:{coord_first_exon_start},{coord_first_exon_end}-{coord_second_exon_start},{coord_second_exon_end}"
-
     return full_coordinate
 
 
 def get_splicing_type(junctions_coords, strand, exons_coords, seqid, gene_name, de_novo_junctions, srr, mean_psi_per_lsv_junction):
+    """
+    Determine the splicing type for each junction based on exon and junction coordinates - ALTD, ALTA and EX.
+
+    Args:
+        junctions_coords (str): String of splicing junction coordinates separated by ';'.
+        strand (str): Strand of the gene ('+' or '-').
+        exons_coords (str): String of exon coordinates separated by ';'.
+        seqid (str): Chromosome or sequence ID.
+        gene_name (str): Name of the gene.
+        de_novo_junctions (str): String of de novo junctions separated by ';' - 1 = de novo; 0 = not de novo.
+        srr (str): Sample SRR identifier.
+        mean_psi_per_lsv_junction (str): String of mean PSI values separated by ';'.
+
+    Returns:
+        tuple: Two dictionaries, `event_table` and `psi_table`, containing splicing event and PSI information.
+    """
     exons_starts = []
     exons_ends = []
     full_coords = []
@@ -67,6 +69,8 @@ def get_splicing_type(junctions_coords, strand, exons_coords, seqid, gene_name, 
     eps = []
     coords = []
     c = 0
+
+    # Parse exon coordinates
     for exon_coord in exons_coords:
         if exon_coord == "":
             break
@@ -74,8 +78,8 @@ def get_splicing_type(junctions_coords, strand, exons_coords, seqid, gene_name, 
         exons_starts.append(exon_coord[0])
         exons_ends.append(exon_coord[1])
 
+    # Analyze each splicing junction
     for junction_coord in junctions_coords:
-
         splice_type = "unknown"
         junction_coord = junction_coord.split('-')
         start = junction_coord[0]
@@ -84,18 +88,23 @@ def get_splicing_type(junctions_coords, strand, exons_coords, seqid, gene_name, 
         full_coords.append(coord_full)
         coords.append(f"{start}-{end}")
         expected_start = expected_end = False
+
+        # Check if the junction start and end are within expected exon boundaries
         if start in exons_ends:
             expected_start = True
         if end in exons_starts:
             expected_end = True
 
         if expected_start and expected_end:
+            # Junction is within expected exon boundaries
             if (int(exons_starts.index(end)) - int(exons_ends.index(start))) > 1:
-                splice_types.append("EX")
+                splice_types.append("EX")  # Exon skipping
             else:
+                # Expected e.g. two consecutive exons
                 splice_types.append("EP")
                 eps.append([start, end, c])
 
+            # Track splicing junction start and end positions
             if start in info["starts"]:
                 info["starts"][start].append(c)
             else:
@@ -107,47 +116,41 @@ def get_splicing_type(junctions_coords, strand, exons_coords, seqid, gene_name, 
                 info["ends"][end] = [c]
 
         else:
-
-            # START ANALYSIS
-
+            # Junction is not within expected exon boundaries (alternative splicing - ALTX type)
             if start not in exons_ends:
                 if strand == "+":
-                    splice_type = "ALTD"
+                    splice_type = "ALTD"  # Alternative donor site
                 elif strand == "-":
-                    splice_type = "ALTA"
+                    splice_type = "ALTA"  # Alternative acceptor site
                 else:
-                    print("Error unknown strand")
-            else:  # if start in expected pos
-                splice_type = "PP"
+                    print("Error: Unknown strand")
+            else:
+                splice_type = "unknown"
 
+            # Track events shared junction start positions
             if start in info["starts"]:
                 shared_positions_index = info["starts"][start]
                 for shared_pos_index in shared_positions_index:
                     other_event_type = splice_types[shared_pos_index]
-                    # if a previous alternative splicing event has been assigned to the coordinates
-                    # both coordinates undergo alternaive splicing
-                    # so ALTX means both ALTD and ALTA
                     if other_event_type != splice_type and other_event_type != "EX" and other_event_type != "EP":
+                        # Both ALTD and ALTA
                         splice_types[shared_pos_index] = "ALTX"
-
                 info["starts"][start].append(c)
-            else:  # if new start
+            else:
                 info["starts"][start] = [c]
 
             splice_types.append(splice_type)
 
-
-# END ANALYSIS
-
+            # Analyze junction end positions
             if end not in exons_starts:
                 if strand == "+":
                     splice_type = "ALTA"
                 elif strand == "-":
                     splice_type = "ALTD"
                 else:
-                    print("Error unknown strand")
-            else:  # if end in expected pos
-                splice_type = "PP"
+                    print("Error: Unknown strand")
+            else:
+                splice_type = "unknown"
 
             if end in info["ends"]:
                 info["ends"][end].append(c)
@@ -157,19 +160,17 @@ def get_splicing_type(junctions_coords, strand, exons_coords, seqid, gene_name, 
             shared_positions_index = info["ends"][end]
             for shared_pos_index in shared_positions_index:
                 other_event_type = splice_types[shared_pos_index]
-                if other_event_type == "PP":
+                if other_event_type == "unknown":
                     splice_types[shared_pos_index] = splice_type
-                elif splice_type == "PP":
+                elif splice_type == "unknown":
                     splice_types[c] = splice_types[c]
-                # if a previous alternative splicing event has been assigned to the coordinates
-                # both coordinates undergo alternaive splicing
-                # so ALTX means both ALTD and ALTA
                 elif other_event_type != splice_type and other_event_type != "EX":
                     splice_types[shared_pos_index] = "ALTX"
                 elif other_event_type != "EX":
                     splice_types[c] = splice_type
         c += 1
 
+    # Handle expected events
     for ep in eps:
         alta = altd = False
         start = ep[0]
@@ -195,6 +196,7 @@ def get_splicing_type(junctions_coords, strand, exons_coords, seqid, gene_name, 
         elif set(splice_types) == {"EP", "ALTX"}:
             splice_types[c] = "ALTX"
 
+    # Create event and PSI tables
     event_table = {}
     psi_table = {}
     k = 0
@@ -202,12 +204,8 @@ def get_splicing_type(junctions_coords, strand, exons_coords, seqid, gene_name, 
     mean_psi_per_lsv_junction = mean_psi_per_lsv_junction.split(";")
     for event in splice_types:
         id_event = f"{gene_name}_{seqid}:{coords[k]}_{strand}_{event}"
-        # print(id_event)
-        # de novo junction?
-
         if id_event in event_table:
-            print("major error")
-        # id, gene, chr, strand, start, end, UCSC coord, event type, MAJIQ, SGSeq, full_co
+            print("Error: Duplicate event ID")
         if len(de_novo) < len(splice_types):
             de_novo = de_novo
         else:
@@ -217,10 +215,27 @@ def get_splicing_type(junctions_coords, strand, exons_coords, seqid, gene_name, 
         psi_table[f"{srr}_{id_event}"] = [de_novo,
                                           mean_psi_per_lsv_junction[k], srr, id_event]
         k += 1
+
     return event_table, psi_table
 
 
 def get_ir_splicing_type(ir_coords, strand, exons_coords, seqid, gene_name, de_novo_junctions, srr, mean_psi_per_lsv_junction):
+    """
+    Determine the splicing type for intron retention events.
+
+    Args:
+        ir_coords (str): String of intron retention coordinates separated by ';'.
+        strand (str): Strand of the gene ('+' or '-').
+        exons_coords (str): String of exon coordinates separated by ';'.
+        seqid (str): Chromosome or sequence ID.
+        gene_name (str): Name of the gene.
+        de_novo_junctions (str): String of de novo junctions separated by ';'.
+        srr (str): Sample SRR identifier.
+        mean_psi_per_lsv_junction (str): String of mean PSI values separated by ';'.
+
+    Returns:
+        tuple: Two dictionaries, `event_table` and `psi_table`, containing intron retention event and PSI information.
+    """
     coords = ir_coords.split(";")
     event_table = {}
     psi_table = {}
@@ -239,18 +254,17 @@ def get_ir_splicing_type(ir_coords, strand, exons_coords, seqid, gene_name, de_n
 
     for coord in coords:
         id_event = f"{gene_name}_{seqid}:{coords[k]}_{strand}_IR"
-        # print(id_event)
-        # de novo junction?
         de_novo = "*"
 
         coord_full = full_coord(exons_starts, exons_ends,
                                 coord.split("-")[0], coord.split("-")[1], seqid)
 
         if id_event in event_table:
-            print("major error")
+            print("Error: Duplicate event ID")
         # id, gene, chr, strand, start, end, UCSC coord, event type, MAJIQ, SGSeq, full co
         event_table[id_event] = [gene_name, seqid, strand,
                                  coords[k].split("-")[0], coord.split("-")[1], f"{seqid}:{coords}", "IR", 1, 0, coord_full]
+        # psi_id, de_novo info, mean psi, srr, id
         psi_table[f"{srr}_{id_event}"] = [de_novo,
                                           mean_psi_per_lsv_junction[k], srr, id_event]
         k += 1
@@ -258,48 +272,46 @@ def get_ir_splicing_type(ir_coords, strand, exons_coords, seqid, gene_name, de_n
     return event_table, psi_table
 
 
-# script
-# voila_info = []
+def majiq_parser(voila_file, srr):
+    """
+    Process MAJIQ output files and generate event and PSI tables.
 
-with open(voila_file, "r") as v:
-    for line in v:
-        if not line.startswith("#"):
-            # print(line)
-            # skip commented lines and header
-            # header
-            # gene_name       gene_id lsv_id  mean_psi_per_lsv_junction       stdev_psi_per_lsv_junction      lsv_type        num_junctions   num_exons       de_novo_junctions       seqid   strand  junctions_coords       exons_coords    ir_coords       ucsc_lsv_link
-            break
-    for line in v:  # Continua lendo as próximas linhas
-        output_events = {}
-        output_psi = {}
-        j = l = m = ""
-        line = line.strip().split("\t")
-        gene_name = line[0]
-        majiq_idx = line[2]
-        mean_psi_per_lsv_junction = line[3]
-        gene_id = line[1]
-        lsv_type = line[5]
-        num_junctions = line[6]
-        num_exons = line[7]
-        de_novo_junctions = line[8]
-        seqid = line[9]
-        strand = line[10]
-        junctions_coords = line[11]
-        exons_coords = line[12]
-        ir_coords = line[13]
+    Args:
+        voila_file (str): Path to the .voila.tsv file from MAJIQ VOILA.
+        srr (str): Sample SRR identifier.
+        output_dir (str): Path to the output directory.
+        output_prefix (str): Prefix for output files.
 
-        if junctions_coords != "":
+    Returns:
+        tuple: Two dictionaries, `output_events` and `output_psi`, containing processed event and PSI information.
+    """
+    with open(voila_file, "r") as v:
+        for line in v:
+            if not line.startswith("#"):
+                break
+        for line in v:
+            output_events = {}
+            output_psi = {}
+            line = line.strip().split("\t")
+            gene_name = line[0]
+            mean_psi_per_lsv_junction = line[3]
+            de_novo_junctions = line[8]
+            seqid = line[9]
+            strand = line[10]
+            junctions_coords = line[11]
+            exons_coords = line[12]
+            ir_coords = line[13]
 
-            output_events_alt, output_psi_alt = get_splicing_type(junctions_coords, strand,
-                                                                  exons_coords, seqid, gene_name, de_novo_junctions, srr, mean_psi_per_lsv_junction)
-            output_events.update(output_events_alt)
-            output_psi.update(output_psi_alt)
+            if junctions_coords != "":
+                output_events_alt, output_psi_alt = get_splicing_type(junctions_coords, strand,
+                                                                      exons_coords, seqid, gene_name, de_novo_junctions, srr, mean_psi_per_lsv_junction)
+                output_events.update(output_events_alt)
+                output_psi.update(output_psi_alt)
 
-        if ir_coords != "":
-            output_events_ir, output_psi_ir = get_ir_splicing_type(ir_coords, strand, exons_coords, seqid,
-                                                                   gene_name, de_novo_junctions, srr, mean_psi_per_lsv_junction)
+            if ir_coords != "":
+                output_events_ir, output_psi_ir = get_ir_splicing_type(ir_coords, strand, exons_coords, seqid,
+                                                                       gene_name, de_novo_junctions, srr, mean_psi_per_lsv_junction)
+                output_events.update(output_events_ir)
+                output_psi.update(output_psi_ir)
 
-            output_events.update(output_events_ir)
-            output_psi.update(output_psi_ir)
-
-        print(output_events, output_psi, sep="\n")  # final outputs
+            return output_events, output_psi
